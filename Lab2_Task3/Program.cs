a3045cf00aca99d3c672619d5d4767aaed6dcbf8
quweth <145272705+quweth@users.noreply.github.com>
2023-11-16 22:18:53 +0200

    Add files via upload

---
+++ Lab2_Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee inform = new Employee();
+            inform.Name = "Vladyslav";
+            inform.Surname = "Zakharchenko";
+            inform.Ocupation = "senior developer";
+            inform.Experience = 5;
+            inform.Amount = 3000;
+            inform.Taxes = 0.3;
+            
+            inform.Info();
+        }
+    }
+}
